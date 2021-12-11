4bbe52d499d9851bab4d9e58b3b372aea2b5fe03
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-12-11 14:36:03 -0500

    Added Loading Scene and Lobby Scene with UI elements. Currently no receiver for Lobby UI fillable texts and loading scene doesn't transition.

---
+++ Assets/ConnectToServer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
